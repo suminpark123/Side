eb98b5365401b7385cd943c80537c597d3b213c9
suminpark123 <a55026851@gmail.com>
2023-04-03 00:45:30 +0900

    커밋

--- WebSocket/server.js
+++ WebSocket/server.js
@@ -2,80 +2,80 @@ const express = require('express');
 const app = express();
 var admin = require("firebase-admin");
 
-// load firebase related information
+// firebase 관련 정보 로드
 var serviceAccount = require("./websocketpro-fc8e3-firebase-adminsdk-c5076-b56d3a8dbf.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-// Initialize Firestore
+// Firestore 초기화
 const db = admin.firestore();
 
-// Send the index.html file to the client
-app.use("/",function(req,res){
-    res.sendFile(__dirname+'/index.html');
+// index.html 파일을 클라이언트에게 전송
+app.use("/", function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-// start server
+// 서버 시작
 const server = app.listen(8080);
 
-// Socket.IO server settings
+// Socket.IO 서버 설정
 const io = require('socket.io')(server);
 
-// Create a Set to store connected clients
+// 연결된 클라이언트를 저장하는 Set 생성
 const clients = new Set();
 
-// when a client connects
-io.on('connection', function(socket) {
-  let username = null; // keep track of the user's username
-  
-  // Add the connected clients to the Set
+// 클라이언트가 연결되었을 때
+io.on('connection', function (socket) {
+  let username = null; // 사용자의 이름을 추적
+
+  // Set에 연결된 클라이언트 추가
   clients.add(socket);
 
-  // send welcome message to new client
-  socket.emit('message', 'start websocket chat');
+  // 새로운 클라이언트에게 환영 메시지 전송
+  socket.emit('message', '웹소켓 채팅 시작');
 
-  socket.on('register', async function(data) {
-    // get the user's desired username from the registration request
+  socket.on('register', async function (data) {
+    // 등록 요청에서 사용자가 원하는 이름 가져오기
     const { username: desiredUsername } = data;
-    
-    // check if the desired username is already taken
+
+    // 원하는 이름이 이미 사용 중인지 확인
     const querySnapshot = await db.collection('users').where('username', '==', desiredUsername).get();
     if (!querySnapshot.empty) {
-      // the username is taken, send an error message to the client
-      socket.emit('registration-error', 'Username already taken');
+      // 이름이 사용 중이면 클라이언트에게 오류 메시지 전송
+      socket.emit('registration-error', '이미 사용 중인 이름입니다.');
       return;
     }
-    
-    // create a new user document in Firestore with the desired username
+
+    // Firestore에 원하는 이름으로 새로운 사용자 문서 생성
     const newUserRef = db.collection('users').doc();
     await newUserRef.set({
       id: newUserRef.id,
       username: desiredUsername
     });
-    
-    // set the username for the current socket and send a success message to the client
+
+    // 현재 소켓에 사용자 이름 설정하고 클라이언트에게 성공 메시지 전송
     username = desiredUsername;
     socket.emit('registration-success', { username });
   });
-  
-  // when receiving chat message from client
-  socket.on('chat-message', function(data) {
+
+  // 클라이언트에서 채팅 메시지를 받았을 때
+  socket.on('chat-message', function (data) {
     if (username) {
-      // if the user has registered, broadcast the chat message with their username
+      // 사용자가 등록했을 경우 사용자 이름과 함께 채팅 메시지를 브로드캐스트
       const message = { username, text: data.text };
       clients.forEach(function each(client) {
         client.emit('chat-message', message);
       });
     } else {
-      // if the user has not registered, send an error message to the client
-      socket.emit('registration-error', 'You must register before sending messages');
+      // 사용자가 등록하지 않았을 경우 클라이언트에게 오류 메시지 전송
+      socket.emit('registration-error', '메시지를 전송하기 전에 등록해야합니다.');
     }
   });
 
-  // when the client closes the connection
-  socket.on('disconnect', function() {
-    // Remove the client that has closed the connection from the Set
+  // 클라이언트가 연결을 종료했을 때
+  socket.on('disconnect', function () {
+    // Set에서 연결을 종료한 클라이언트 제거
     clients.delete(socket);
   });
 });
